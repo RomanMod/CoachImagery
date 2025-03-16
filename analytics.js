1225462dd55d06ebb60f562ebf35eef17b6800fd
MoodRa <58891836+RomanMod@users.noreply.github.com>
2025-03-16 19:48:55 +0200

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,5 @@
+// analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-X4QJLM5LTM'); // Ваш ID Google Analytics
