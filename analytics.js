c7bb363c082f2cca14cfa1cdfdab6ebe4e1abbef
MoodRa <58891836+RomanMod@users.noreply.github.com>
2025-03-16 20:07:16 +0200

    Update analytics.js
    Удалил файл analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,1 @@
-// analytics.js
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-X4QJLM5LTM'); // Ваш ID Google Analytics
+
